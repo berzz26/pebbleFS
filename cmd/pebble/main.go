688e2dcddc45251586d50ab9f2ff3b518ae06357
berzz26 <aumtamboli2233@gmail.com>
2026-08-02 17:58:47 +0000

    feat: enhance main function with improved command handling and initialization messages to test the initial iteration of pebble

--- cmd/pebble/main.go
+++ cmd/pebble/main.go
@@ -2,19 +2,46 @@ package main
 
 import (
 	"fmt"
-	"github.com/berzz/pebbleFS/internal/service"
 	"log"
+	"os"
+
+	"github.com/berzz/pebbleFS/internal/service"
 )
 
 func main() {
-	fmt.Println("Initializing PebbleFS")
 
 	app, err := service.New()
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	defer app.Metadata.Close()
 
-	fmt.Println("PebbleFS initialized successfully")
+	// TODO: Make this configurable later.
+	if err := app.Storage.AddDisk("/mnt/usb"); err != nil {
+		log.Fatal(err)
+	}
+
+	if len(os.Args) < 2 {
+		fmt.Println("Usage:")
+		fmt.Println("  pebble put <file>")
+		return
+	}
+
+	switch os.Args[1] {
+
+	case "put":
+
+		if len(os.Args) < 3 {
+			log.Fatal("missing file path")
+		}
+
+		if err := app.Put(os.Args[2]); err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println("Upload completed successfully.")
+
+	default:
+		log.Fatalf("unknown command: %s", os.Args[1])
+	}
 }
