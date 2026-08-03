84346c9f3c32e514457b112a90f60c4ce7bc8732
berzz26 <aumtamboli2233@gmail.com>
2026-08-03 04:26:17 +0000

    feat: implement the cli tool for delete functionaltiy

--- cmd/pebble/delete.go
+++ cmd/pebble/delete.go
@@ -1,1 +1,29 @@
 package main
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var deleteCmd = &cobra.Command{
+	Use:   "delete <file>",
+	Short: "Delete a stored file",
+	Args:  cobra.ExactArgs(1),
+
+	RunE: func(cmd *cobra.Command, args []string) error {
+
+		err := app.Delete(args[0])
+		if err != nil {
+			return err
+		}
+
+		fmt.Println("File deleted successfully.")
+
+		return nil
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(deleteCmd)
+}
